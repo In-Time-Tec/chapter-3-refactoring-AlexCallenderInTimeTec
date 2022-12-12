a4e5f4e22952e5d81b49e80a8d98b48baaf73965
Alex Callender <alex.callender@intimetec.com>
2022-12-12 10:48:13 -0700

    refactored Print method.

--- TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -8,26 +8,40 @@ namespace TheatricalPlayersRefactoringKata
     {
         public string Print(Invoice invoice, Dictionary<string, Play> plays)
         {
-            var totalAmount = 0;
-            var volumeCredits = 0;
-            var result = string.Format("Statement for {0}\n", invoice.Customer);
-            CultureInfo cultureInfo = new CultureInfo("en-US");
+            int totalAmount = PerformanceAmount();
+            int volumeCredits = PeformanceAmount();
+            string customerInvoiceStatement = CustomerInvoiceStatement(invoice);
+            NewPerformance(invoice, plays, ref totalAmount, ref volumeCredits, ref customerInvoiceStatement);
+            customerInvoiceStatement = PerformanceResults(totalAmount, volumeCredits, customerInvoiceStatement);
+            return customerInvoiceStatement;
+        }
 
-            foreach(var perf in invoice.Performances) 
+        private static string PerformanceResults(int totalAmount, int volumeCredits, string result)
+        {
+            result += String.Format(SetCultureInfo(), "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
+            result += String.Format("You earned {0} credits\n", volumeCredits);
+            return result;
+        }
+
+        private static void NewPerformance(Invoice invoice, Dictionary<string, Play> plays, ref int totalAmount, ref int volumeCredits, ref string result)
+        {
+            foreach (var perf in invoice.Performances)
             {
                 var play = plays[perf.PlayID];
                 var thisAmount = 0;
-                switch (play.Type) 
+                switch (play.Type)
                 {
                     case "tragedy":
                         thisAmount = 40000;
-                        if (perf.Audience > 30) {
+                        if (perf.Audience > 30)
+                        {
                             thisAmount += 1000 * (perf.Audience - 30);
                         }
                         break;
                     case "comedy":
                         thisAmount = 30000;
-                        if (perf.Audience > 20) {
+                        if (perf.Audience > 20)
+                        {
                             thisAmount += 10000 + 500 * (perf.Audience - 20);
                         }
                         thisAmount += 300 * perf.Audience;
@@ -41,12 +55,29 @@ namespace TheatricalPlayersRefactoringKata
                 if ("comedy" == play.Type) volumeCredits += (int)Math.Floor((decimal)perf.Audience / 5);
 
                 // print line for this order
-                result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
+                result += String.Format(SetCultureInfo(), "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
                 totalAmount += thisAmount;
             }
-            result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
-            result += String.Format("You earned {0} credits\n", volumeCredits);
-            return result;
+        }
+
+        private static string CustomerInvoiceStatement(Invoice invoice)
+        {
+            return string.Format("Statement for {0}\n", invoice.Customer);
+        }
+
+        private static int PeformanceAmount()
+        {
+            return 0;
+        }
+
+        private static int PerformanceAmount()
+        {
+            return 0;
+        }
+
+        private static CultureInfo SetCultureInfo()
+        {
+            return new CultureInfo("en-US");
         }
     }
 }
